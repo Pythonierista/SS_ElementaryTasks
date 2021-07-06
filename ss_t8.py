5d86b2189874bd3da63afcf59590148d101b398a
Danylo <danylo_liashchuk@epam.com>
2021-07-07 01:54:25 +0300

    Almost

--- ss_t8.py
+++ ss_t8.py
@@ -7,27 +7,28 @@ class Fibonacci:
         self.s = start
         self.e = end
 
-    def f(self):
+    def create_fibonacci_array(self):
         lst = [0, 1, 1]
         while self.s > lst[0]:
-            lst = lst[1:].append(lst[-1] + lst[-2])
+            lst.append(lst[-1] + lst[-2])
+            lst = lst[1:]
         while self.e > lst[-1]:
-            lst = lst.append(lst[-1] + lst[-2])
-        return lst
+            lst.append(lst[-1] + lst[-2])
+        return lst[:-1]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('start')
-    parser.add_argument('end')
+    parser.add_argument('start', default='1')
+    parser.add_argument('end', default='1')
     args = parser.parse_args()
 
     try:
         args.start, args.end = float(args.start), float(args.end)
-        if args.start <= args.end:
-            print(*Fibonacci(args.start, args.end).f())
+        if args.start < args.end:
+            print(*Fibonacci(args.start, args.end).create_fibonacci_array())
         else:
-            print('First number must be less or equal to the second')
+            print('First number must be less than the second')
     except ValueError:
         print('Please enter digits to calculate Fibonacci sequence')
 
