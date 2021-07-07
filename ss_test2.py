fd10135570f2566c78bfdf90387610657cc97ddd
Danylo <danylo_liashchuk@epam.com>
2021-07-07 21:43:36 +0300

    demo

---
+++ ss_test2.py
@@ -0,0 +1,27 @@
+import unittest
+from ss_t2 import *
+
+
+class TestEnvelope (unittest.TestCase):
+
+    def test_not_fitting_ints(self):
+        env1 = Envelope(2, 2)
+        env2 = Envelope(1, 3)
+        self.assertFalse(env1 < env2)
+        self.assertFalse(env1 > env2)
+
+    def test_not_fitting_with_floats(self):
+        env1 = Envelope(2.2, 2)
+        env2 = Envelope(1, 3.1)
+        self.assertFalse(env1 < env2)
+        self.assertFalse(env1 > env2)
+
+    def test_first_inside_second(self):
+        env1 = Envelope(2, 5)
+        env2 = Envelope(10, 11)
+        self.assertTrue(env1 < env2)
+        self.assertFalse(env1 > env2)
+
+
+if __name__ == '__main__':
+    unittest.main()
