055ed81e9e79ba19bf068d7ef37d18b6dad2a8a0
Danylo <danylo_liashchuk@epam.com>
2021-07-07 17:48:06 +0300

    refactored

--- ss_t2.py
+++ ss_t2.py
@@ -1,16 +1,40 @@
-n = 'y'
-while n.lower() in ['y', 'yes']:
-    try:
-        a1 = float(input('1 side of the 1 envelope: '))
-        a2 = float(input('2 side of the 1 envelope: '))
-        b1 = float(input('1 side of the 2 envelope: '))
-        b2 = float(input('2 side of the 2 envelope: '))
-        eq1 = (a1 >= b1 and a2 >= b2) or (a1 >= b2 and a2 >= b1)
-        eq2 = (b1 >= a1 and b2 >= a2) or (b1 >= a2 and b2 >= a1)
-        if eq1 or eq2:
-            print('Yes you can put the envelope in the other one')
-        else:
-            print('No no, cannot do that')
-        n = input('Wanna continue? ')
-    except ValueError:
-        print('Write integer parameters')
+"""Есть два конверта со сторонами (a,b) и (c,d) определить, можно ли один конверт вложить в другой. Программа должна
+обрабатывать ввод чисел с плавающей точкой. Программа спрашивает у пользователя размеры конвертов по одному параметру
+за раз. После каждого подсчёта программа спрашивает у пользователя хочет ли он продолжить. Если пользователь ответит
+“y” или “yes” (без учёта регистра), программа продолжает работу сначала, в противном случае – завершает выполнение. """
+
+
+class Envelope:
+
+    def __init__(self, side_a, side_b):
+        self.a = side_a
+        self.b = side_b
+
+    def __lt__(self, other):
+        return self.a < other.a and self.b < other.b or self.a < other.b and self.b < other.a
+
+    def __gt__(self, other):
+        return self.a > other.a and self.b > other.b or self.a > other.b and self.b > other.a
+
+
+def main():
+    n = 'y'
+    while n.lower() in ['y', 'yes']:
+        try:
+            a = float(input('1 side of the 1 envelope: '))
+            b = float(input('2 side of the 1 envelope: '))
+            c = float(input('1 side of the 2 envelope: '))
+            d = float(input('2 side of the 2 envelope: '))
+            env1 = Envelope(a, b)
+            env2 = Envelope(c, d)
+            if env1 > env2 or env1 < env2:
+                print('Yes you can put the envelope in the other one')
+            else:
+                print('No no, cannot do that')
+            n = input('Wanna continue? ')
+        except ValueError:
+            print('Write integer parameters')
+
+
+if __name__ == '__main__':
+    main()
